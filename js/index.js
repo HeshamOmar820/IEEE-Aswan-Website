10fb13a9a06dbb283d067fa48735393cac0c44f8
Hesham Omar <heshamomar820@gmail.com>
2020-08-06 16:13:57 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 $(window).load(function () {
 
     "use strict";
-    $(".loading-overlay .spinner").fadeOut(2500, function () {
+    $(".loading-overlay .spinner").fadeOut(2000, function () {
 
         $("body").css("overflow", "auto"); //Showing scroll
+        $("body").css("overflow-x", "hidden");
         $(this).parent().fadeOut(1000, function () {
 
             $(this).remove();
         });
 
     });
-
+    
 });
