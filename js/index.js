a97d71d94be99cbd1005db4e053ee2731c0c2375
Hesham <heshamomar820@gmail.com>
2020-08-16 18:53:28 +0200

    final touch

--- js/index.js
+++ js/index.js
@@ -2,6 +2,14 @@ var scrollButton = $("#scroll-top")
 $(window).load(function () {
 
     "use strict";
+    $("html").niceScroll({
+        cursorcolor:"black",
+        cursorwidth:"10px",
+        cursorborderradius:0,
+        cursorborder:0,
+        background:"rgba(20,20,20,0.3)",
+        
+    });
     $(".loading-overlay .spinner").fadeOut(2000, function () {
 
         $("body").css("overflow", "auto"); //Showing scroll
